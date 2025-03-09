c5ece11d8547bd73aa8b1940d175e9081e7edbe0
SIJIE MEI <49129114+SijieMei@users.noreply.github.com>
2025-03-09 14:43:56 -0500

    feat: add frontend

--- electron/index.js
+++ electron/index.js
@@ -1,25 +1,28 @@
 document.onkeydown = updateKey;
 document.onkeyup = resetKey;
 
-var server_port = 65432;
-var server_addr = "192.168.3.49";   // the IP address of your Raspberry PI
+var server_port = 65426;
+var server_addr = "192.168.137.65";   // the IP address of your Raspberry PI
+var mainIntervalId = null
+var intervalId = null
 
 function client(){
     
     const net = require('net');
-    var input = document.getElementById("message").value;
 
     const client = net.createConnection({ port: server_port, host: server_addr }, () => {
         // 'connect' listener.
         console.log('connected to server!');
-        // send the message
-        client.write(`${input}\r\n`);
+        client.write(`connecting\r\n`);
     });
     
     // get the data from the server
     client.on('data', (data) => {
-        document.getElementById("bluetooth").innerHTML = data;
-        console.log(data.toString());
+        if(data){
+            const { temp } = JSON.parse(data.toString())
+            document.getElementById("temperature").innerHTML = temp ?? '0';
+            console.log(data.toString());
+        }
         client.end();
         client.destroy();
     });
@@ -27,34 +30,67 @@ function client(){
     client.on('end', () => {
         console.log('disconnected from server');
     });
+}
 
+function send_data(keyCode) {
+    const net = require('net');
+    
+    const client = net.createConnection({ port: server_port, host: server_addr }, () => {
+        console.log('connected to server!');
+        client.write(`${keyCode}\r\n`);
+    });
 
+    client.on('data', (data) => {
+        if(data){
+            const { key, power } = JSON.parse(data.toString())
+            document.getElementById("direction").innerHTML = key ?? '0.0';
+            document.getElementById("speed").innerHTML = power ?? '0.0';
+            if(key == "\r\n") {
+                document.getElementById("direction").innerHTML = "Stop";
+                document.getElementById("speed").innerHTML = '0.0';
+            }
+            console.log(data.toString());
+        }
+        client.end();
+        client.destroy();
+    });
 }
 
 // for detecting which key is been pressed w,a,s,d
 function updateKey(e) {
-
+    if(mainIntervalId !== null && intervalId !== null) {
+        return 
+    }
+    
     e = e || window.event;
-
+    let keyCode = null
     if (e.keyCode == '87') {
         // up (w)
         document.getElementById("upArrow").style.color = "green";
-        send_data("87");
+        keyCode = "up";
     }
     else if (e.keyCode == '83') {
         // down (s)
         document.getElementById("downArrow").style.color = "green";
-        send_data("83");
+        keyCode = "down";
     }
     else if (e.keyCode == '65') {
         // left (a)
         document.getElementById("leftArrow").style.color = "green";
-        send_data("65");
+        keyCode = "left";
     }
     else if (e.keyCode == '68') {
         // right (d)
         document.getElementById("rightArrow").style.color = "green";
-        send_data("68");
+        keyCode = "right";
+    }
+
+    if (keyCode) {
+        send_data(keyCode);
+        intervalId = setInterval(() => {
+            console.log(1111)
+            send_data(keyCode);
+        }, 500);
     }
 }
 
@@ -62,6 +98,13 @@ function updateKey(e) {
 function resetKey(e) {
 
     e = e || window.event;
+
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+
+    send_data('')
 
     document.getElementById("upArrow").style.color = "grey";
     document.getElementById("downArrow").style.color = "grey";
@@ -72,8 +115,15 @@ function resetKey(e) {
 
 // update data for every 50ms
 function update_data(){
-    setInterval(function(){
-        // get image from python server
-        client();
-    }, 50);
+    if(mainIntervalId !== null) {
+        document.getElementById("start_button").innerHTML = "Start monitor";     
+        clearInterval(mainIntervalId);
+        mainIntervalId = null;
+    } else {
+        document.getElementById("start_button").innerHTML = "Monitering...";
+        mainIntervalId = setInterval(function(){
+            // get image from python server
+            client();
+        }, 500);
+    }
 }
--- frontend_tutorial/wifi_server.py
+++ frontend_tutorial/wifi_server.py
@@ -1,7 +1,8 @@
 import socket
+import json
 
-HOST = "192.168.3.49" # IP address of your Raspberry PI
-PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+HOST = "192.168.137.1" # IP address of your Raspberry PI
+PORT = 65430           # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -14,7 +15,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             if data != b"":
                 print(data)     
-                client.sendall(data) # Echo back to client
+                client.sendall(json.dumps({ "direction": 1, "speed":1, "temperature":2 }).encode()) # Echo back to client
     except: 
         print("Closing socket")
         client.close()
